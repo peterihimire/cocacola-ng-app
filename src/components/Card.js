4087a2b072e12afab908b710be9bdd332c6f1601
Peter Ihimire <peterihimire@gmail.com>
2021-02-24 12:26:59 +0100

    Added coca-cola  location marker to the map

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,29 @@
 import React from "react";
 import "./Card.css";
 import map from "../assets/map.png";
+import location2 from "../assets/location-2.svg";
 
 const Card = () => {
   return (
     <div className="card">
-      <img src={map} alt="card" className="card-img" />
+      <div className="map-img">
+        <img src={map} alt="card" className="card-img" />
+      </div>
       <div className="card-footer">
         <h3>Cocacola Depots</h3>
         <p>26 location posts</p>
+      </div>
+      <div className="cola-mark">
+        <img src={location2} alt="card" className="location-2" />
+      </div>
+      <div className="cola-mark">
+        <img src={location2} alt="card" className="location-1" />
+      </div>
+      <div className="cola-mark">
+        <img src={location2} alt="card" className="location-3" />
+      </div>
+      <div className="cola-mark">
+        <img src={location2} alt="card" className="location-4" />
       </div>
     </div>
   );
